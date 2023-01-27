dc5d3134073f282d490155759292149708b45343
Mahmoud Gamal <Mahmoud_8383@rocketmail.com>
2023-01-27 17:27:28 +0200

    7SEG

---
+++ 7-Segmant/Code/7-Segmant/main.c
@@ -0,0 +1,42 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 26, 2023
+ *      Author: MGAMAL15
+ */
+#include"BIT_OP.h"
+#include"STD_TYPES.h"
+#include"DIO_Interface.h"
+#define F_CPU 8000000UL //1MHz Clock frequency
+#include <util/delay.h>  // For delay function
+
+int main()
+{
+	SET_Direction(PORT_A,PIN_2,INPUT);  
+	SET_Direction_4LSB(PORT_C,OUTPUT);  
+ 
+	SET_Value(PORT_A, PIN_2, HIGH); 
+	
+	u8 Current_number =0;
+	
+	while(1)
+	{
+		
+		if(GET_Value(PORT_A,PIN_2) == LOW)
+		   {
+			   if(Current_number == 9) //to reset numbers to zero after reach 9
+			      {
+				    Current_number = 0;
+			      }
+			   else
+			      {
+			 	    Current_number++;
+			      }
+		   }
+      
+	  SET_Value_4LSB(PORT_C, Current_number);
+	  _delay_ms(200);   // we use delay function to solve debouncing problem
+	}
+	return 0;
+}
+
